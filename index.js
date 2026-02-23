f3063da80d07657352cb88222067e1762b925282
aayushxjoshi <aayushj9173@gmail.com>
2026-02-23 23:00:53 +0530

    fixes

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ class LoggerService {
   }
 
   log(level, message, meta = {}) {
-    // Only log if message level >= configured level
     if (this.levels.indexOf(level) <= this.levels.indexOf(this.level)) {
       const timestamp = new Date().toISOString();
       const logMsg = `[${timestamp}] [${level.toUpperCase()}] ${message}`;
 
-      // Choose console method based on log level
       switch (level) {
         case "error":
           console.error(logMsg, Object.keys(meta).length ? meta : "");
@@ -24,13 +22,11 @@ class LoggerService {
     }
   }
 
-  // Convenience methods
   info(msg, meta) { this.log("info", msg, meta); }
   error(msg, meta) { this.log("error", msg, meta); }
   warn(msg, meta) { this.log("warn", msg, meta); }
   debug(msg, meta) { this.log("debug", msg, meta); }
 
-  // Dynamically update log level (e.g. from DB)
   setLevel(newLevel) {
     if (this.levels.includes(newLevel)) {
       this.level = newLevel;
@@ -40,9 +36,6 @@ class LoggerService {
     }
   }
 }
-
-
- //Created a single shared instance (singleton) Controlled via environment variable LOG_LEVEL
 
 const logger = new LoggerService(process.env.LOG_LEVEL || "info");
 
